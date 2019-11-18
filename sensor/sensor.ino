a7f1c0c2d5e3304c624fa7b60c8283f557a9674d
Pedro <p.mariano95@yahoo.com.br>
2019-11-17 21:55:26 -0300

    adptando sensor para deteccao de som

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,16 +1,32 @@
 //Definicao pino sensor
-int pino_analogico = 33;
+int pino_analogico = 36;
+int buzzer_pin = 12;
+int channel = 0;
+int frequence = 2000;
+int resolution = 10;
 
 int valor_A0 = 0;
 
 void setup(){  
   Serial.begin(1200);  
   //Define pinos sensor como entrada  
-  pinMode(pino_analogico, INPUT);  
+  pinMode(pino_analogico, INPUT);
+  delay(1000); // give me time to bring up serial monitor    
+  ledcSetup(channel, frequence, resolution); //configuração de canal, frequencia e resolução
+  ledcAttachPin(buzzer_pin, channel); 
 }
 
-void loop(){  
+void loop(){
+  
   valor_A0 = analogRead(pino_analogico);  
   Serial.print("Saida A0: ");  
   Serial.println(valor_A0);
+  ledcWriteTone(channel, frequence);
+
+  if (valor_A0 >= 1600 || valor_A0 <= 1000) {
+     ledcWrite(channel, 400);
+  } else {
+    ledcWriteTone(channel, 0);
+  }
+  
 }
